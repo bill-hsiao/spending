05b0cc88a8584996c349198dc6749db49fe13ccd
Bill Hsiao <billhsia@usc.edu>
2019-05-27 13:27:46 -0700

    r10 error port environment varia le

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 const app = require('./application')
 
-const port = 5000
+const port = process.env.PORT || 80
 
 app.listen(port, () => console.log(`API server started on ${port}`))
