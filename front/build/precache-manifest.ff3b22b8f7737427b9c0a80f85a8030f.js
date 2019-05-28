4d99b04840822ee9353250bb798f497f168e4cf0
Bill Hsiao <billhsia@usc.edu>
2019-05-27 21:31:24 -0700

    build folder was in gitignore

---
+++ front/build/precache-manifest.ff3b22b8f7737427b9c0a80f85a8030f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943fdf85bdaf5ce1f8d124b13d3eaaf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c466a0cfaa8999b0c09a",
+    "url": "/static/css/main.698b187a.chunk.css"
+  },
+  {
+    "revision": "8d2882b33b790c59354a",
+    "url": "/static/js/2.24c59d4d.chunk.js"
+  },
+  {
+    "revision": "c466a0cfaa8999b0c09a",
+    "url": "/static/js/main.fb687b89.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
